e70920e64a8f294f7e0ff623251a1699c165f4f9
AK Zaber <zaberkhan611@gmail.com>
2026-02-28 18:48:51 +0600

    modal implemented

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ const loadCategoriesData = () => {
         })
 }
 
+const loadModal = (id) => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => showDetails(data))
+}
+
+const showDetails = (product) => {
+    const getDiv = document.getElementById('modal-details');
+    getDiv.innerHTML = `
+        <div class="space-y-5">
+            <div class="p-4 bg-slate-100 rounded flex items-center justify-center">
+                <img class="w-1/2" src=${product.image}>
+            </div>
+            <div class="flex justify-between">
+                <button class="btn btn-sm">${product.category}</button>
+                <div>
+                    <p>${product.rating.rate}(${product.rating.count})</p>
+                </div>
+            </div>
+            <div class="space-y-5">
+                <h2 class="text-2xl font-bold">${product.title}</h2>
+                <p>${product.description}</p>
+            </div>
+        </div>
+    `;
+    document.getElementById('details_modal').showModal();
+}
+
 const loadProducts = () => {
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
@@ -39,11 +67,11 @@ const showProducts = (products) => {
         <div class="badge badge-md bg-slate-100">${product.category}</div>
         <div><p>${product.rating.rate}(${product.rating.count})</p></div>
     </div>
-    <h2 class="card-title">${product.title}</h2>
-    <h1 class="text-4xl font-bold">$ ${product.price}</h1>
+    <h2 class="card-title truncate min-w-0 line-clamp-2">${product.title}</h2>
+    <h1 class="text-3xl font-bold">$ ${product.price}</h1>
     <div class="card-actions justify-between">
-      <button class="btn btn-primary">Details</button>
-      <button class="btn btn-primary">Add</button>
+      <button onclick="loadModal(${product.id})" class="btn"><i class="fa-regular fa-eye"></i>Details</button>
+      <button class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i>Add</button>
     </div>
   </div>
 </div>
@@ -52,4 +80,5 @@ const showProducts = (products) => {
     })
 }
 
+loadProducts();
 loadCategoriesData();
